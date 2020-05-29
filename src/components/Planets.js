17384d2f31b486a2eb07c64f783d59ea96e682d7
javiarch <javidiaz1977@gmail.com>
2020-05-28 21:42:03 -0300

    cambio en Planets.js

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -14,22 +14,32 @@ const PLANETS = gql`
     }
   }
 `;
-
 const Planets = ({ newPlanets }) => {
   	const { loading, error, data } = useQuery(PLANETS);
+	const alertName = () => {
+    		alert(' John Doe ');
+  	};
+
+	//const [shoot] = () => { window.console.log('ejecutando select') }
+	//const onItemClick = ({landmark, e}) => { console.log(landmark)};
+		
 
 	const renderPlanets = (landmark) => {
+		//let boundItemClick = this.onItemClick.bind(this, landmark);		
+		
+
 		return landmark.map(({ id, name, type }) => (
-      		<ListItem key={id}>
+      		<ListItem key={id} onClick={alertName}>
         		{name} <Badge>{type}</Badge>
       		</ListItem>
     		));
 	}
 
+
   	if (loading) return <p>Loading ...</p>;
   	if (error) return <p>Error :(</p>;
   
-  /*return(
+  	/*return(
 	<List>
 		{data.landmark.map(({id, name, type, location}) => (	
   		<ListItem key={id}>
@@ -43,5 +53,38 @@ const Planets = ({ newPlanets }) => {
 	)*/
     	return <List>{renderPlanets(newPlanets || data.landmark)}</List>;
 };
+
+//con class components no me funciono
+/*class Planets extends React.Component {
+	constructor(props) {
+    	super(props)
+	this.state = {
+		newPlanets: "Arno"
+	}
+  	}
+
+	render(){
+		window.console.log('this.state ->', this.state);
+		
+		return (
+			<List>
+				<renderPlanets />
+			</List>			
+		);
+	}	
+}
+
+//COMO RECIBO landmark ACÁ???? como prop??
+class renderPlanets extends React.Component {
+  constructor(props) {
+    	super(props)	
+  }
+  
+  render() {
+    	return (
+		<p>'renderPlanets'</p>
+	);    
+  }
+}*/
 
 export default Planets;
